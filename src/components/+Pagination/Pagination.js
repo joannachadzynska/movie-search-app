c13db19b726ce78af581995b77298668df5e01a8
Joanna Chądzyńska <jmalek87@gmail.com>
2020-03-11 20:19:37 +0100

    updated Pagination component: styles and functions

--- src/components/+Pagination/Pagination.js
+++ src/components/+Pagination/Pagination.js
@@ -1,10 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { connect } from "react-redux";
-import { getMovies } from "../../duck/movies/actions";
-import PageNumber from "./PageNumber";
-
-const LEFT_PAGE = "LEFT";
-const RIGHT_PAGE = "RIGHT";
+import React from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import PageButton from "./PageButton";
 
 // function* range(start, end) {
 // 	while (start < end) {
@@ -13,136 +9,90 @@ const RIGHT_PAGE = "RIGHT";
 // 	}
 // }
 
-const range = (from, to, step = 1) => {
-	let i = from;
-	const range = [];
+const Pagination = ({ totalPages, currentPage, pagesToShow, onPageChange }) => {
+	const halfPagerCount = (totalPages - 1) / 2;
 
-	while (i <= to) {
-		range.push(i);
-		i += step;
-	}
+	let showPrevMore = false;
+	let showNextMore = false;
 
-	return range;
-};
-
-const Pagination = ({
-	searchedValue,
-	totalPages,
-	currentPage,
-	getMovies,
-	onChangePage,
-	pageNeighbours
-}) => {
-	const [activePage, setActivePAge] = useState(1);
-	const pageNeighboursNum =
-		typeof pageNeighbours === "number"
-			? Math.max(0, Math.min(pageNeighbours, 2))
-			: 0;
-
-	const pageNumbers = Array.from({ length: totalPages }, (v, k) => k + 1);
-
-	const goToPage = (page) => {
-		const currentPage = Math.max(0, Math.min(page, totalPages));
-		const paginationData = {
-			currentPage,
-			totalPages
-		};
-
-		setActivePAge(page);
-	};
-
-	const first = () => getMovies(searchedValue, 1);
-
-	const previous = () => {
-		goToPage(activePage, pageNeighbours * 2 - 1);
-		getMovies(searchedValue, currentPage - 1);
-	};
-
-	const next = () => {
-		goToPage(activePage - pageNeighbours * 2 + 1);
-		getMovies(searchedValue, currentPage + 1);
-	};
-
-	const last = () => getMovies(searchedValue, totalPages);
-
-	const current = (e, page) => {
-		e.preventDefault();
-		goToPage(page);
-		getMovies(searchedValue, page);
-	};
-
-	const fetchPageNumbers = () => {
-		const totalNumbers = pageNeighboursNum * 2 + 1;
-		const totalBlocks = totalNumbers + 1;
-
-		if (totalPages > totalBlocks) {
-			let pages = [];
-
-			const leftBound = currentPage - pageNeighboursNum;
-			const rightBound = currentPage + pageNeighboursNum;
-			const beforeLastPage = totalPages - 1;
-
-			const startPage = leftBound > 2 ? leftBound : 2;
-			const endPage = rightBound < beforeLastPage ? rightBound : beforeLastPage;
-
-			pages = range(startPage, endPage);
-
-			const pagesCount = pages.length;
-			const singleSpillOffset = totalNumbers - pagesCount - 1;
-
-			const leftSpill = startPage > 2;
-			const rightSpill = endPage < beforeLastPage;
-
-			const leftSpillPage = LEFT_PAGE;
-			const rightSpillPage = RIGHT_PAGE;
-
-			if (leftSpill && !rightSpill) {
-				const extraPages = range(startPage - singleSpillOffset, startPage - 1);
-				pages = [leftSpillPage, ...extraPages, ...pages];
-			} else if (!leftSpill && rightSpill) {
-				const extraPages = range(endPage + 1, endPage + singleSpillOffset);
-				pages = [...pages, ...extraPages, rightSpillPage];
-			} else if (leftSpill && rightSpill) {
-				pages = [leftSpillPage, ...pages, rightSpillPage];
-			}
-
-			return [1, ...pages, totalPages];
+	if (totalPages > pagesToShow) {
+		if (currentPage > pagesToShow - halfPagerCount) {
+			showPrevMore = true;
 		}
 
-		return range(1, totalPages);
-	};
+		if (currentPage < totalPages - halfPagerCount) {
+			showNextMore = true;
+		}
+	}
 
-	const pages = fetchPageNumbers();
-	if (totalPages === 1) return null;
+	const pagesLinks = [];
+
+	if (showPrevMore && !showNextMore) {
+		const startPage = totalPages - (pagesToShow - 2);
+		for (let i = startPage; i < totalPages; i += 1) {
+			pagesLinks.push(i);
+		}
+	} else if (!showPrevMore && showNextMore) {
+		for (let i = 2; i < pagesToShow; i += 1) {
+			pagesLinks.push(i);
+		}
+	} else if (showPrevMore && showNextMore) {
+		const offset = Math.floor(pagesToShow / 2) - 1;
+
+		for (let i = currentPage - offset; i <= currentPage + offset; i += 1) {
+			pagesLinks.push(i);
+		}
+	} else {
+		for (let i = 2; i < totalPages; i += 1) {
+			pagesLinks.push(i);
+		}
+	}
+
+	const first = () => onPageChange(1);
+	const previous = () => onPageChange(currentPage - 1);
+	const next = () => onPageChange(currentPage + 1);
+	const last = () => onPageChange(totalPages);
+	const current = (page) => onPageChange(page);
 
 	return (
 		<nav className='search__btns'>
 			<ul className='pagination'>
-				<li>
-					<button
-						onClick={first}
-						className={currentPage === 1 ? "disabled" : ""}>
-						First
-					</button>
-				</li>
-				<li>
-					<button
-						onClick={previous}
-						className={currentPage === 1 ? "disabled" : ""}>
-						Previous
-					</button>
-				</li>
+				<PageButton
+					onPress={previous}
+					classes={currentPage === 1 ? "disabled" : ""}>
+					<IoIosArrowBack />
+				</PageButton>
 
-				{pages.map((page, index) => {
-					return (
-						<PageNumber
-							key={page}
-							page={page}
-							isCurrentPage={page === currentPage}
-							onPress={(e) => current(e, page)}
-						/>
-					);
-				})}
+				<PageButton
+					onPress={first}
+					classes={currentPage === 1 ? "disabled active" : ""}>
+					1
+				</PageButton>
+
+				{showPrevMore && (
+					<PageButton
+						onPress={previous}
+						classes={currentPage === 1 ? "disabled" : ""}>
+						...
+					</PageButton>
+				)}
+
+				{pagesLinks.map((page) => (
+					<PageButton
+						key={page}
+						onPress={() => current(page)}
+						classes={page === currentPage ? "active " : ""}>
+						{page}
+					</PageButton>
+				))}
+
+				{showNextMore && (
+					<PageButton
+						onPress={next}
+						classes={currentPage === 1 ? "disabled " : ""}>
+						...
+					</PageButton>
+				)}
 
 				{/* {totalPages !== null
 					? pageNumbers.map((page) => (
@@ -155,33 +105,20 @@ const Pagination = ({
 					  ))
 					: null} */}
 
-				<li>
-					<button
-						onClick={next}
-						className={currentPage === totalPages ? "disabled" : ""}>
-						Next
-					</button>
-				</li>
-				<li>
-					<button
-						onClick={last}
-						className={currentPage === totalPages ? "disabled" : ""}>
-						Last
-					</button>
-				</li>
+				<PageButton
+					onPress={last}
+					classes={currentPage === totalPages ? "disabled active" : ""}>
+					{totalPages}
+				</PageButton>
+
+				<PageButton
+					onPress={next}
+					classes={currentPage === 1 ? "disabled " : ""}>
+					<IoIosArrowForward />
+				</PageButton>
 			</ul>
 		</nav>
 	);
 };
 
-const mapState = ({ movies, similar }) => ({
-	movies: movies,
-	totalPages: movies.totalPages,
-	currentPage: movies.currentPage
-});
-
-const mapDispatch = (dispatch) => ({
-	getMovies: (query, pageNumber) => dispatch(getMovies(query, pageNumber))
-});
-
-export default connect(mapState, mapDispatch)(Pagination);
+export default Pagination;
