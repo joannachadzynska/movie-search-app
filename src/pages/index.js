d20dbb843bd8dd1ce4b68aa3906fb58af5507065
Joanna Chądzyńska <jmalek87@gmail.com>
2020-03-13 16:06:35 +0100

    added barrelling into pages component

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Favorite from "./+Favorite";
+import LandingPage from "./+LandingPage";
+import NotFound from "./+NotFound";
+import SignIn from "./+SignIn";
+import SignOut from "./+SignOut";
+import SignUp from ".//+SignUp";
+import Watchlist from "./+Watchlist";
+
+export { Favorite, LandingPage, NotFound, SignIn, SignOut, SignUp, Watchlist };
