211027ba83b7cf29e8eb2aac507f4a9de3c2c678
Joanna Chądzyńska <jmalek87@gmail.com>
2020-03-17 20:15:05 +0100

    updated filters component by removing some stuff

--- client/src/components/+Filters/Filters.js
+++ client/src/components/+Filters/Filters.js
@@ -1,33 +1,9 @@
 import React from "react";
 import CustomSelect from "../+CustomSelect";
 
-const Filters = ({ setSearchTerm, setIsSelectMedia, items }) => {
-	const mediaTypeOptions = ["movie", "tv", "person"];
-	const sortOptions = ["rating", "year", "title", "name"];
-	// const [searchTerm, setSearchTerm] = useState("");
-	// const [isSelectMedia, setIsSelectMedia] = useState(false);
-
-	const selectMediaType = (e) => {
-		e.persist();
-		setIsSelectMedia(true);
-		setSearchTerm(e.target.value);
-	};
-
-	const sortByRating = () => {
-		// sort by rating
-
-		console.log(items.map((el) => el.item.vote_average));
-
-		items.sort((a, b) => b.item.vote_average - a.item.vote_average);
-	};
-
-	const sortBy = (e) => {
-		e.persist();
-		const { value } = e.target;
-		if (value === "rating") {
-			sortByRating();
-		}
-	};
+const Filters = ({ items, sortBy, getFilterValue }) => {
+	const mediaTypeOptions = ["all", "movie", "tv", "person"];
+	const sortOptions = ["best rating", "lowest rating", "year", "name"];
 
 	return (
 		<>
@@ -35,7 +11,7 @@ const Filters = ({ setSearchTerm, setIsSelectMedia, items }) => {
 				options={mediaTypeOptions}
 				name='mediaType'
 				id='mediaType'
-				handleChange={selectMediaType}
+				handleChange={getFilterValue}
 			/>
 			<CustomSelect
 				options={sortOptions}
