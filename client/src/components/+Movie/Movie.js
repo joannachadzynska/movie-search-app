0213cca7add8440c100e3616b4e6f6d323db553b
Joanna Chądzyńska <jmalek87@gmail.com>
2020-05-03 16:13:30 +0200

    added new rating components

--- client/src/components/+Movie/Movie.js
+++ client/src/components/+Movie/Movie.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -8,8 +8,10 @@ import {
 	setImgUrl,
 	setProfileImgUrl,
 	getOnlyYear,
-	setRating
+	setRating,
 } from "../../utils/utils";
+import RatingStars from "../+RatingBox/RatingStars";
+import RatingButton from "../+RatingBox/RatingButton";
 
 const Movie = ({
 	movie: {
@@ -20,16 +22,17 @@ const Movie = ({
 		first_air_date: airDate,
 		poster_path,
 		profile_path,
-		media_type: type
+		media_type: type,
 	},
 	movie,
 	getMediaType,
-	rating
+	rating,
 }) => {
 	let image;
 	let cardName;
-
+	const [selectedItemId, setSelectedItemId] = useState("");
 	const favorites = useSelector((state) => state.favorites.watched);
+	const watchlist = useSelector((state) => state.watchlist.toWatch);
 	const user = useSelector((state) => state.user);
 
 	const setImgType = () => {
@@ -62,54 +65,67 @@ const Movie = ({
 		switch (type) {
 			case "movie":
 				return (
-					<p className='card__text'>
-						Released: {getOnlyYear(release)}, Type: {type}
-					</p>
+					<div className='card__date'>Released: {getOnlyYear(release)}</div>
 				);
 
 			case "tv":
 				return (
-					<p className='card__text'>
-						Released: {airDate ? getOnlyYear(airDate) : "no info"}, Type: {type}
-					</p>
+					<div className='card__date'>
+						Released: {airDate ? getOnlyYear(airDate) : "no info"}
+					</div>
 				);
 
 			case "person":
-				return <p className='card__text'>Type: {type}</p>;
+				return <div className='card__date'>Type: {type}</div>;
 
 			default:
-				return <p className='card__text'>No info</p>;
+				return <div className='card__date'>No info</div>;
 		}
 	};
 
 	const link = `/details/${id}`;
 
+	const isInWatchlist = watchlist.some((item) => item.id === movie.id);
+
 	return (
 		<li className='cards__item'>
 			<div className='card'>
-				<div className='card-image__container'>
-					<Link to={link} onClick={() => getMediaType(type)}>
-						<img
-							src={
-								poster_path === null || profile_path === null
-									? customPoster
-									: setImgType()
-							}
-							alt={`The movie titled: ${title}`}
-						/>
-					</Link>
-				</div>
+				<Link to={link} onClick={() => getMediaType(type)}>
+					<img
+						src={
+							poster_path === null || profile_path === null
+								? customPoster
+								: setImgType()
+						}
+						alt={`The movie titled: ${title}`}
+					/>
+				</Link>
 
 				<div className='card__block'>
-					<h4 className='card__title'>{setCardName()}</h4>
-					{setCardText(type)}
+					<div className='card__meta'>
+						<div className='card__title'>{setCardName()}</div>
 
-					<RatingBox
+						{setCardText(type)}
+					</div>
+
+					<RatingButton
+						selectedItemId={selectedItemId}
+						setItem={setSelectedItemId}
+						isInWatchlist={isInWatchlist}
+						item={movie}
+					/>
+
+					{/* <RatingBox
 						userRating={setRating(favorites, id, rating)}
 						userId={user.isAuthenticated ? user.email : null}
 						item={movie}
-					/>
+					/> */}
 				</div>
+				<RatingStars
+					userRating={setRating(favorites, id, rating)}
+					userId={user.isAuthenticated ? user.email : null}
+					item={movie}
+				/>
 			</div>
 		</li>
 	);
